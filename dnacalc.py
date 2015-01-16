ceedee0ce5c72f00ac4d3361eb0178034094d78e
Galen Scovell <galen.scovell@gmail.com>
2015-01-15 22:03:54 -0800

    Update dnacalc to PEP8 Conventions

--- dnacalc.py
+++ dnacalc.py
@@ -12,43 +12,43 @@
 import sys
 
 
-def contentCalculation(seq, nt):
+def content_calculation(seq, nt):
     print('\nCalculating %s content in: \n%s\n' % (nt, seq))
     nt_content = seq.count(nt)
     total_length = len(seq)
     ratio = (nt_content / total_length) * 100
     print('\tSequence length: %d \n\tNucleotide Amount: %d \n\tPercentage: %.2f\n' % (total_length, nt_content, ratio))
 
-def nucleotideChoice():
+def nucleotide_choice():
     nucleotide = ' '
     while nucleotide[0] not in ('A', 'T', 'C', 'G'): 
         print('What is the nucleotide of interest?')
         nucleotide = input(' > ').upper()
     return nucleotide[0]
 
-def filterSequence(seq):
+def filter_sequence(seq):
     filtered_sequence = []
     for character in seq:
         if character in ('A', 'T', 'C', 'G'):
             filtered_sequence.append(character)
     return ''.join(filtered_sequence)
 
-def promptInput():
+def prompt_input():
     print('\nWhat is the DNA sequence of interest?')
     sequence = input(' > ').upper()
-    sequence = filterSequence(sequence)
+    sequence = filter_sequence(sequence)
     print('\nFiltered sequence: %s\n' % sequence)
-    contentCalculation(sequence, nucleotideChoice())
+    content_calculation(sequence, nucleotide_choice())
     sys.exit()
 
 
 def main():
     # Uncomment only the following line to run prompt input
-    # promptInput()
+    # prompt_input()
 
     if len(sys.argv) == 3:
         sequence, nucleotide = sys.argv[1].upper(), sys.argv[2][0].upper()
-        contentCalculation(filterSequence(sequence), nucleotide)
+        content_calculation(filter_sequence(sequence), nucleotide)
     else:
         print("\n\tTwo arguments required (sequence, nucleotide).\n")
         sys.exit()
